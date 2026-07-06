a32f689afd0c0366126183d7406b7dd02f5c818a
komalshukla902-creator <komalshukla902@gmail.com>
2026-07-06 17:50:50 +0530

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,9 @@
+<!-- signup.html -->
+<form action="signup.php" method="POST">
+    <h2>Sign Up</h2>
+    <input type="text" name="username" placeholder="Choose Username" required>
+    <input type="password" name="password" placeholder="Choose Password" required>
+    <button type="submit">Register</button>
+</form>
+<p>Already have an account? <a href="login.html">Login here</a></p>
+
